d9db70eb46d126d7836058ab0379ab7dd26e69db
jkru3 <josephkrueger@protonmail.com>
2024-02-15 20:44:39 -0800

    first commit

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 conda create -n protoqa python=3.6
-source activate protoqa
-git clone git@github.com:iesl/protoqa-evaluator.git
+source conda activate protoqa
+git clone https://github.com/jkru3/ProtoQA.git
 pip install -e protoqa-evaluator
 conda install pytorch=1.4.0 torchvision torchaudio cudatoolkit=10.2 -c pytorch
-git clone git@github.com:huggingface/transformers.git
+git clone https://github.com/huggingface/transformers.git
 cd transformers
 git checkout tags/v2.1.1
 pip install -e .
