f4a804c1ade22e517a7a9d49e98aaaccde7d9faa
Joey Laurent Krueger III <josephkrueger@protonmail.com>
2024-02-15 21:22:42 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 conda create -n protoqa python=3.6
 source conda activate protoqa
-git clone https://github.com/jkru3/ProtoQA.git
+git clone https://github.com/jkru3/ProtoQA_GPT3.git
 pip install -e protoqa-evaluator
 conda install pytorch=1.4.0 torchvision torchaudio cudatoolkit=10.2 -c pytorch
 git clone https://github.com/huggingface/transformers.git
